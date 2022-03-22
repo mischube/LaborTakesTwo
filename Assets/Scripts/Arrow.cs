7cd87a7c7d5f0f2e6692fa911831dc56d85869c3
SchnitzelMS <50582299+SchnitzelMS@users.noreply.github.com>
2022-03-22 16:15:59 +0100

    code cleanup

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -7,10 +7,9 @@ public class Arrow : MonoBehaviourPun
     public PlattformMovement plattformMovement;
 
     public Vector3 direction;
-    
+
     private void OnTriggerStay(Collider other)
     {
-        Debug.Log("triggered");
         if (other.transform.CompareTag("Player"))
         {
             plattformMovement.MoveInDirection(direction);
