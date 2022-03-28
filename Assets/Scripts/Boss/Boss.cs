9f3e5299e1f05f40b80a08bf5d480480d477ae88
Dennis Weber <dennisweber5@outlook.de>
2022-03-28 14:22:35 +0200

    First Boss impl

---
+++ Assets/Scripts/Boss/Boss.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    private float health = 3f;
+    private float fireBallCd = 5f;
+    private bool fireBallActive = false;
+
+    private void Update()
+    {
+        if (!fireBallActive)
+        {
+            StartCoroutine(ShootFireBall());
+        }
+    }
+
+    private IEnumerator ShootFireBall()
+    {
+        fireBallActive = true;
+        yield return new WaitForSeconds(fireBallCd);
+        fireBallActive = false;
+    }
+}
