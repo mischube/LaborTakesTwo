4824bdbb4f7d1a865a6863aac13421c20c68ef41
Sascha <ajustus@stud.hs-heilbronn.de>
2022-04-17 17:11:28 +0200

    Inventory

--- Assets/Scripts/Interactable Objects/PlayerInventory.cs
+++ Assets/Scripts/Interactable Objects/PlayerInventory.cs
@@ -7,24 +7,29 @@ public class PlayerInventory : MonoBehaviour
 {
     private List<Transform> _weapons = new List<Transform>();
     private Transform _baseWeaponPosition;
-    public int _selectedWeapon= 0;
+    [SerializeField]
+    private int _selectedWeapon= 0;
     private InteractableInfo _interactable;
+    [SerializeField]
+    private GameObject _uiEnabler;
     private GameObject _pickUpWeapon;
     private bool onInit = false;
     private void PickupWeapon()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (!onInit)
+            var inventory = _uiEnabler.GetComponent<UIVisibility>();
+            _interactable = transform.parent.gameObject.GetComponent<InteractableInfo>();
+            if (!onInit && _interactable.itemHoveredNow)
             {
                 foreach (Transform weapon in transform)
                 {
                     _weapons.Add(weapon);
                 }
                 SetHandPosition();
+                inventory.EnableAllUI();
                 onInit = true;
             }
-            _interactable = transform.parent.gameObject.GetComponent<InteractableInfo>();
             if (_interactable.itemHoveredNow)
             {
                 _pickUpWeapon = _interactable.GetHoveredItem();
@@ -34,6 +39,10 @@ public class PlayerInventory : MonoBehaviour
                 _pickUpWeapon.transform.position = _baseWeaponPosition.transform.position;
                 ClearHands();
                 _pickUpWeapon.gameObject.SetActive(true);
+                if(_weapons.Count == 3)
+                    inventory.TurnUIElementsAround(2);
+                if(_weapons.Count == 4)
+                    inventory.TurnUIElementsAround(3);
             }
         }
     }
@@ -63,11 +72,13 @@ public class PlayerInventory : MonoBehaviour
 
     private void SelectWeapon()
     {
+        var inventory = _uiEnabler.GetComponent<UIVisibility>();
         int i = 0;
         foreach (Transform weapon  in _weapons)
         {
             if (i == _selectedWeapon)
             {
+                inventory.TurnUIElementsAround(i);
                 weapon.gameObject.SetActive(true);
             }
             else
