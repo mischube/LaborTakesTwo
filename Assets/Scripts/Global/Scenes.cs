130e286e51164cd14e372ab1c5ef72ed5d3c412d
SchnitzelMS <50582299+SchnitzelMS@users.noreply.github.com>
2022-04-18 15:26:01 +0200

    added enum for scenes names

---
+++ Assets/Scripts/Global/Scenes.cs
@@ -0,0 +1,12 @@
+using Library.StringEnums;
+
+namespace Global
+{
+    public enum Scenes
+    {
+        [StringEnum("Prototype")] Prototype,
+        [StringEnum("Tutorial")] Tutorial,
+        [StringEnum("Advanced")] Advanced,
+        [StringEnum("Boss")] Boss
+    }
+}
