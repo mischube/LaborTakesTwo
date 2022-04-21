a1c0bb720d8ed6a218f739b8084e77a6088aecf6
SchnitzelMS <50582299+SchnitzelMS@users.noreply.github.com>
2022-04-21 21:05:19 +0200

    moved file into correct namespace

--- Assets/Scripts/UI/InteractableInfo.cs
+++ Assets/Scripts/UI/InteractableInfo.cs
@@ -1,51 +1,54 @@
 using Photon.Pun;
 using UnityEngine;
 
-public class InteractableInfo : MonoBehaviourPun
+namespace UI
 {
-    public float interactionDistance;
-    public Camera cam;
-    public LayerMask interactableMask;
-    public bool itemHoveredNow;
-
-    private RaycastHit _hit;
-    private GameObject _hitObject;
-    private bool _itemHoveredOnce;
-    private Ray _ray;
-    private TextBoxScript _textBoxScript;
-
-    void Update()
+    public class InteractableInfo : MonoBehaviourPun
     {
-        _ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
-        if (Physics.Raycast(_ray, out _hit, interactionDistance, interactableMask))
+        public float interactionDistance;
+        public Camera cam;
+        public LayerMask interactableMask;
+        public bool itemHoveredNow;
+
+        private RaycastHit _hit;
+        private GameObject _hitObject;
+        private bool _itemHoveredOnce;
+        private Ray _ray;
+        private TextBoxScript _textBoxScript;
+
+        void Update()
         {
-            _textBoxScript = _hit.transform.gameObject.GetComponent<TextBoxScript>();
-            _hitObject = _hit.transform.gameObject;
-            SetInteractableTextRotation();
-            ShowInteractableText();
-            _itemHoveredOnce = true;
-            itemHoveredNow = true;
+            _ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+            if (Physics.Raycast(_ray, out _hit, interactionDistance, interactableMask))
+            {
+                _textBoxScript = _hit.transform.gameObject.GetComponent<TextBoxScript>();
+                _hitObject = _hit.transform.gameObject;
+                SetInteractableTextRotation();
+                ShowInteractableText();
+                _itemHoveredOnce = true;
+                itemHoveredNow = true;
+            }
+
+            if (_itemHoveredOnce && !Physics.Raycast(_ray, out _hit, interactionDistance, interactableMask))
+            {
+                _textBoxScript.SetDescription("");
+                itemHoveredNow = false;
+            }
         }
 
-        if (_itemHoveredOnce && !Physics.Raycast(_ray, out _hit, interactionDistance, interactableMask))
+        void ShowInteractableText()
         {
-            _textBoxScript.SetDescription("");
-            itemHoveredNow = false;
+            _textBoxScript.SetDescription(_textBoxScript.GetDescription());
         }
-    }
 
-    void ShowInteractableText()
-    {
-        _textBoxScript.SetDescription(_textBoxScript.GetDescription());
-    }
+        void SetInteractableTextRotation()
+        {
+            _textBoxScript.SetTextRotation(cam.transform.rotation);
+        }
 
-    void SetInteractableTextRotation()
-    {
-        _textBoxScript.SetTextRotation(cam.transform.rotation);
-    }
-
-    public GameObject GetHoveredItem()
-    {
-        return _hitObject;
+        public GameObject GetHoveredItem()
+        {
+            return _hitObject;
+        }
     }
 }
