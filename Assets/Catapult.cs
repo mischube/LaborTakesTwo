7d6af008692cb9d6c8fed0711c4f509fc0badc40
Dennis Weber <dennisweber5@outlook.de>
2022-05-02 09:29:43 +0200

    First catapult steps

---
+++ Assets/Catapult.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : MonoBehaviour
+{
+    public GameObject rod;
+
+    public void CatapultSwing()
+    {
+        
+    }
+}
