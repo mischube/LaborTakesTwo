4882afb551d8258a2246cb014c6b21c28cfea88e
Dennis Weber <dennisweber5@outlook.de>
2022-05-09 15:39:39 +0200

    only shoots one Cannonball now

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -1,15 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 
-public class Cannon : MonoBehaviour
+public class Cannon : MonoBehaviourPun
 {
     public GameObject cannonBallPrefab;
     public Transform cannonBallShootingPosition;
 
     public void ShootCannonBall()
     {
+        if (!photonView.IsMine)
+            return;
+        
         PhotonNetwork.Instantiate
             (cannonBallPrefab.name, cannonBallShootingPosition.position, transform.rotation);
     }
