fedfba727b6caeaa0fbbfc25ff901f0bf2c1af2f
SchnitzelMS <50582299+SchnitzelMS@users.noreply.github.com>
2022-05-14 12:01:25 +0200

    moved class to namespace

--- Assets/Scripts/UI/HealthbarUI.cs
+++ Assets/Scripts/UI/HealthbarUI.cs
@@ -2,56 +2,61 @@ using System.Collections.Generic;
 using Player;
 using UnityEngine;
 
-public class HealthbarUI : MonoBehaviour
+namespace UI
 {
-    public GameObject heartPrefab;
-    public RectTransform heartSpawnPoint;
-
-    private PlayerHealth playerHealth;
-    [SerializeField]
-    private List<GameObject> heartList = new List<GameObject>(); 
-
-    private void Start()
+    public class HealthbarUI : MonoBehaviour
     {
-        playerHealth = PlayerNetworking.LocalPlayerInstance.GetComponent<PlayerHealth>();
-        playerHealth.PlayerDmgEvent += LoseAHeart;
-        playerHealth.PlayerDeadEvent += FillAllHearts;
-        CreateHearts();
-    }
+        public GameObject heartPrefab;
+        public RectTransform heartSpawnPoint;
 
-    private void CreateHearts()
-    {
-        int width = Screen.width;
-        float spaceSide = (float) width / 16;
+        private PlayerHealth playerHealth;
+        [SerializeField] private List<GameObject> heartList = new List<GameObject>();
 
-        int height = Screen.height;
-        float spaceTop = (float) height / 25;
-        
-        for (int i = 0; i < playerHealth.GetMaxHealth(); i++)
+        private void Start()
         {
-            GameObject heartInstantiate = 
-                Instantiate(heartPrefab, heartSpawnPoint.position + new Vector3(spaceSide * 1.5f + i * spaceSide,-spaceTop,0),
-                    heartSpawnPoint.rotation, transform);
-            heartList.Add(heartInstantiate);
+            playerHealth = PlayerNetworking.LocalPlayerInstance.GetComponent<PlayerHealth>();
+            playerHealth.PlayerDmgEvent += LoseAHeart;
+            playerHealth.PlayerDeadEvent += FillAllHearts;
+            CreateHearts();
         }
-    }
 
-    public void LoseAHeart(int currentHealth)
-    {
-        heartList[currentHealth].gameObject.transform.GetChild(0).gameObject.SetActive(false);
-    }
-
-    public void FillAllHearts()
-    {
-        foreach (var heart in heartList)
+        private void CreateHearts()
         {
-            heart.transform.GetChild(0).gameObject.SetActive(true);
-        }
-    }
+            int width = Screen.width;
+            float spaceSide = (float)width / 16;
 
-    private void OnDisable()
-    {
-        playerHealth.PlayerDmgEvent -= LoseAHeart;
-        playerHealth.PlayerDeadEvent -= FillAllHearts;
+            int height = Screen.height;
+            float spaceTop = (float)height / 25;
+
+            for (int i = 0; i < playerHealth.GetMaxHealth(); i++)
+            {
+                GameObject heartInstantiate =
+                    Instantiate(
+                        heartPrefab,
+                        heartSpawnPoint.position + new Vector3(spaceSide * 1.5f + i * spaceSide, -spaceTop, 0),
+                        heartSpawnPoint.rotation,
+                        transform);
+                heartList.Add(heartInstantiate);
+            }
+        }
+
+        public void LoseAHeart(int currentHealth)
+        {
+            heartList[currentHealth].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+        public void FillAllHearts()
+        {
+            foreach (var heart in heartList)
+            {
+                heart.transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+
+        private void OnDisable()
+        {
+            playerHealth.PlayerDmgEvent -= LoseAHeart;
+            playerHealth.PlayerDeadEvent -= FillAllHearts;
+        }
     }
 }
